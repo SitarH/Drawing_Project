cdafd7ed5536f23de653ff4179b88ee488cf257a
Sitar Hirshberg <sitar55h@gmail.com>
2022-04-02 09:29:24 +0300

    add client

--- server/index.js
+++ server/index.js
@@ -1,3 +1,36 @@
-import express from 'express';
+const express = require('express');
+
+const axios = require('axios')
+
+const PORT = process.env.PORT || 5005;
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.json('hi')
+})
+
+// server.get('/data', (req, res) => {
+//     const options = {
+//         method: 'GET',
+//         params: {word: 'bla'},
+//         headers: {'Content-Type': 'application/json'}
+//       }
+    
+//       axios.request(options).then((response) =>{
+//           res.json(response.data)
+//           console.log(response.data)
+        
+//       }).catch((err) =>{
+//           console.error(err)
+//       })
+// })
+
+
+
+
+
+
+server.listen(PORT, ()=> {console.log(`http://localhost:${PORT}`)});
 
 
